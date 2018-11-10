86c18aa754f98ce12f2fd0276f34afb888b71fd7
Anna Przybycien <ania.przybycien@gmail.com>
2018-11-10 10:02:57 +0100

    reee

--- server.R
+++ server.R
@@ -1,152 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-library(plotly)
-
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  }, bg="transparent")
-  
-  output$plot <- renderPlotly({
-    # specify some map projection/options
-    g <- list(
-      scope = 'usa',
-      projection = list(type = 'albers usa'),
-      lakecolor = toRGB('white')
-    )
-    plot_ly(z = state.area, text = state.name, locations = state.abb,
-            type = 'choropleth', locationmode = 'USA-states') %>%
-      layout(geo = g)
-  })
-  output$plott <- renderPlotly({
-    # specify some map projection/options
-    g <- list(
-      scope = 'usa',
-      projection = list(type = 'albers usa'),
-      lakecolor = toRGB('white')
-    )
-    plot_ly(z = state.area, text = state.name, locations = state.abb,
-            type = 'choropleth', locationmode = 'USA-states') %>%
-      layout(geo = g)
-  })
-  
-  
-  
-})
-=======
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(plotly)
 library(ggplot2)
-
-# Electricity data reading (for all areas)
-el_data <- read.csv('data/el_data.csv')
-el_data <- el_data[el_data$X.Location. == 'ALLAREA',]
-
-# Homicides data reading (16.1.1 indicator)
-hom_data <- read.csv('data/homicides_data.csv')
-hom_data <- hom_data[hom_data$Units == 'NUMBER',]
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  # Electricity reactive values and button reactions
-  rv.el_data <- reactiveValues(
-    y = el_data[el_data$GeoAreaName == 'Poland',]$Value,
-    x = el_data[el_data$GeoAreaName == 'Poland',]$TimePeriod
-  )
-  observeEvent(input$el_poland, {
-    rv.el_data$y = el_data[el_data$GeoAreaName == 'Poland',]$Value
-    rv.el_data$x = el_data[el_data$GeoAreaName == 'Poland',]$TimePeriod
-  })
-  observeEvent(input$el_brazil, {
-    rv.el_data$y = el_data[el_data$GeoAreaName == 'Brazil',]$Value
-    rv.el_data$x = el_data[el_data$GeoAreaName == 'Brazil',]$TimePeriod
-  })
-  observeEvent(input$el_afghanistan, {
-    rv.el_data$y = el_data[el_data$GeoAreaName == 'Afghanistan',]$Value
-    rv.el_data$x = el_data[el_data$GeoAreaName == 'Afghanistan',]$TimePeriod
-  })
-  
-  rv.hom_data <- reactiveValues(
-    y = hom_data$Value[hom_data$GeoAreaName == 'Poland'],
-    x = hom_data$TimePeriod[hom_data$GeoAreaName == 'Poland']
-  )
-  observeEvent(input$hom_poland, {
-    rv.hom_data$y = hom_data$Value[hom_data$GeoAreaName == 'Poland']
-    rv.hom_data$x = hom_data$TimePeriod[hom_data$GeoAreaName == 'Poland']
-  })
-  observeEvent(input$hom_germany, {
-    rv.hom_data$y = hom_data$Value[hom_data$GeoAreaName == 'Germany']
-    rv.hom_data$x = hom_data$TimePeriod[hom_data$GeoAreaName == 'Germany']
-  })
-  observeEvent(input$hom_afghanistan, {
-    rv.hom_data$y = hom_data$Value[hom_data$GeoAreaName == 'Afghanistan']
-    rv.hom_data$x = hom_data$TimePeriod[hom_data$GeoAreaName == 'Afghanistan']
-  })
-  
-  # Electricity plot
-  output$elPlot <- renderPlot({
-    temp <- data.frame(x = rv.el_data$x, y = rv.el_data$y)
-    ggplot(data=temp, aes(x=x, y=y, group=1)) +
-      geom_line()+
-      geom_point()+
-      ylim(0, 100)+
-      ylab('%')+
-      xlab('year')+
-      geom_area(alpha=0.5, fill='red')+
-      theme(plot.background = element_rect(fill="#F7F7EF"))
-  })
-  
-  output$homPlot <- renderPlot({
-    temp <- data.frame(x = rv.hom_data$x, y = rv.hom_data$y)
-    ggplot(data=temp, aes(x=x, y=y, group=1)) +
-      geom_line()+
-      geom_point()
-  })
-})
->>>>>>> cdbd8b65391a6d3de18950a606e5b45445a32bad
-=======
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-library(plotly)
-library(ggplot2)
+library(rworldmap)
 
 # Electricity data reading (for all areas)
 el_data <- read.csv('data/el_data.csv')
@@ -213,5 +69,20 @@ shinyServer(function(input, output) {
       geom_line()+
       geom_point()
   })
+  output$worldMap<-renderPlot({
+    map.world <- map_data(map="world")
+    
+    #Add the data you want to map countries by to map.world
+    #In this example, I add lengths of country names plus some offset
+    map.world$name_len <- nchar(map.world$region) + sample(nrow(map.world))
+    
+    gg <- ggplot()
+    gg <- gg + theme(legend.position="none")
+    gg <- gg + geom_map(data=map.world, map=map.world, aes(map_id=region, x=long, y=lat, fill=name_len))
+    
+    gg <- gg + scale_fill_gradient(low = "green", high = "brown3", guide = "colourbar")
+    gg <- gg + coord_equal()
+    gg
+  })
 })
->>>>>>> 555a7d2f2aba8fb756556711a8f8d4fd348a3db1
+
--- ui.R
+++ ui.R
@@ -1,104 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
-library(plotly)
-
-
-htmlTemplate("index.html",
-             button = actionButton("action", "Action"),
-             slider = sliderInput("x", "X", 1, 100, 50),
-             slider_input = sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30),
-             web_plot = plotOutput("distPlot"),
-             plotly_plot = plotlyOutput("plott"),
-             web_plotly = plotlyOutput("plot")
-             
-         
-)
-
-# Define UI for application that draws a histogram
-#shinyUI(fluidPage(
-  
-  # Application title
-#  titlePanel("Old Faithful Geyser Data"),
-  
-#  htmlTemplate("index.html", name = "component1"),
-  # Sidebar with a slider input for number of bins 
-#  sidebarLayout(
-#    sidebarPanel(
-#       sliderInput("bins",
-#                   "Number of bins:",
-#                   min = 1,
-#                   max = 50,
-#                   value = 30)
-#    ),
-    
-    # Show a plot of the generated distribution
-#    mainPanel(
-#       plotOutput("distPlot")
-#    )
-#  )
-#))
-=======
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-library(plotly)
-
-htmlTemplate("index.html",
-             # Electricity plot and buttons
-             el_plot = plotOutput("elPlot"),
-             el_button.Poland = actionButton("el_poland", "Poland"),
-             el_button.Brazil = actionButton("el_brazil", "Brazil"),
-             el_button.Afghanistan = actionButton("el_afghanistan", "Afghanistan"),
-             
-             hom_plot = plotOutput("homPlot"),
-             hom_button.Poland = actionButton("hom_poland", "Poland"),
-             hom_button.Germany = actionButton("hom_germany", "Germany"),
-             hom_button.Afghanistan = actionButton("hom_afghanistan", "Afghanistan")
-)
-
-# Define UI for application that draws a histogram
-#shinyUI(fluidPage(
-  
-  # Application title
-#  titlePanel("Old Faithful Geyser Data"),
-  
-#  htmlTemplate("index.html", name = "component1"),
-  # Sidebar with a slider input for number of bins 
-#  sidebarLayout(
-#    sidebarPanel(
-#       sliderInput("bins",
-#                   "Number of bins:",
-#                   min = 1,
-#                   max = 50,
-#                   value = 30)
-#    ),
-    
-    # Show a plot of the generated distribution
-#    mainPanel(
-#       plotOutput("distPlot")
-#    )
-#  )
-#))
->>>>>>> cdbd8b65391a6d3de18950a606e5b45445a32bad
-=======
-#
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
@@ -120,7 +20,8 @@ htmlTemplate("index.html",
              hom_plot = plotOutput("homPlot"),
              hom_button.Poland = actionButton("hom_poland", "Poland"),
              hom_button.Germany = actionButton("hom_germany", "Germany"),
-             hom_button.Afghanistan = actionButton("hom_afghanistan", "Afghanistan")
+             hom_button.Afghanistan = actionButton("hom_afghanistan", "Afghanistan"),
+             worldMap=plotOutput("worldMap")
 )
 
 # Define UI for application that draws a histogram
@@ -146,4 +47,3 @@ htmlTemplate("index.html",
 #    )
 #  )
 #))
->>>>>>> 555a7d2f2aba8fb756556711a8f8d4fd348a3db1
